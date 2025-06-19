632fae21923cd2a62e603e754b41a7ff592f4db6
Khawar Sultan <khawarsultan663@gmail.com>
2025-06-19 14:53:59 +0500

    Add dotenv configuration to authClient for environment variable management; set path to .env.local for local development.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from 'better-auth/react';
-
+import { config } from "dotenv";
+config({ path: ".env.local" });
 // export const authClient = createAuthClient({
 //     baseURL: process.env.BETTER_AUTH_URL || 'http://localhost:3000',
 // });
