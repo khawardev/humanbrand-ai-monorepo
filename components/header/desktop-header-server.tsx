38dd688332dd21d8782d4823320bba5487f20f6b
Khawar Sultan <khawarsultan663@gmail.com>
2025-06-25 14:46:37 +0500

    Refactor SessionsList and DesktopHeaderServer components to enhance rendering logic. Reintroduce skeleton loading in SessionsList for improved user experience when no saved sessions are available, while simplifying the DesktopHeaderServer by removing unnecessary suspense wrapping around SavedSessions.

--- components/header/desktop-header-server.tsx
+++ components/header/desktop-header-server.tsx
@@ -2,9 +2,7 @@ import UserMenu from "@/components/header/user-menu";
 import SavedSessions from "../aiag-components/reusable-components/session/saved-sessions";
 import { SigninButtonDesktop } from "../aiag-components/reusable-components/auth/signin-button";
 import MobileHeader from "./mobile-header";
-import { Suspense } from "react";
 import { getSession } from "@/lib/get-session";
-import { getUserWithSavedSessions } from "@/actions/users-actions";
 
 export const revalidate = 0;
 
@@ -21,12 +19,9 @@ const DesktopHeaderServer = async () => {
         return <SigninButtonDesktop />;
     }
 
-
     return (
         <>
-            <Suspense fallback={<Skeleton />}>
-                <SavedSessions />
-            </Suspense>
+            <SavedSessions />
             <UserMenu user={session?.user} />
             <div className="sm:hidden block pl-2 border-l">
                 <MobileHeader user={session?.user} />
