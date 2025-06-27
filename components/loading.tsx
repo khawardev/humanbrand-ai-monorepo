04103c0955f16103e03cb9dc93757941999da66a
Khawar Sultan <khawarsultan663@gmail.com>
2025-06-27 23:13:16 +0500

    Refactor Loading component to replace loader icon with a ping animation for improved visual feedback. Update globals.css to remove unnecessary whitespace at the end of the file.

--- components/loading.tsx
+++ components/loading.tsx
@@ -4,7 +4,14 @@ import { LuLoaderCircle } from 'react-icons/lu'
 
 const Loading = () => {
     return (
-        <div className='flex-center min-h-[90vh]'> <LuLoaderCircle className="text-muted-foreground size-4 animate-spin" /></div>
+        <div className='flex-center min-h-[90vh]'>
+            {/* <LuLoaderCircle className="text-muted-foreground size-4 animate-spin" /> */}
+            <span className="relative flex size-3">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75"></span>
+                <span className="relative inline-flex size-3 rounded-full bg-primary"></span>
+            </span>
+
+        </div>
     )
 }
 
