d6a9c55efb0511aeeb02576f71f335d7359136fa
Khawar Sultan <khawarsultan663@gmail.com>
2025-08-08 12:54:27 +0500

    Add console logging for BETTER_AUTH_URL and GOOGLE_CLIENT_SECRET in HomePage component to assist with debugging environment variable configurations.

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import { Hero } from "@/components/aiag-components/reusable-components/hero"
 import AdminMailAlert from "@/components/aiag-components/admin-mail-alert"
 import HomePageComponent from "@/components/aiag-components/home/home-page-component"
 export default function HomePage() {
+  console.log(process.env.BETTER_AUTH_URL, `BETTER_AUTH_URL`);
+  console.log(process.env.GOOGLE_CLIENT_SECRET, `GOOGLE_CLIENT_SECRET`);
   return (
     <main className="pt-14 relative">
+      
       <Hero />
       <section className="div-center-md">
         <AdminMailAlert />
