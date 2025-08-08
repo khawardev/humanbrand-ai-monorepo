0ccfe0d8225a6cf459d73352c44ac41c782089cb
Khawar Sultan <khawarsultan663@gmail.com>
2025-08-08 15:16:39 +0500

    Update Google clientId assignment in better-auth to use optional chaining for improved safety in environment variable access.

--- lib/better-auth/auth.ts
+++ lib/better-auth/auth.ts
@@ -13,7 +13,7 @@ export const auth = betterAuth({
     },
     socialProviders: {
         google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientId: process?.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         },
     },
