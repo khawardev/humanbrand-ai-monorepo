3ff9811820d234a7d9da665c5a402253522f7f3b
Khawar Sultan <khawarsultan663@gmail.com>
2025-08-08 15:24:29 +0500

    Remove console logging of BETTER_AUTH_URL and GOOGLE_CLIENT_SECRET from HomePage component to enhance security and reduce unnecessary output.

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,8 @@ import { Hero } from "@/components/aiag-components/reusable-components/hero"
 import AdminMailAlert from "@/components/aiag-components/admin-mail-alert"
 import HomePageComponent from "@/components/aiag-components/home/home-page-component"
 export default function HomePage() {
-  console.log(process.env.BETTER_AUTH_URL, `BETTER_AUTH_URL`);
-  console.log(process.env.GOOGLE_CLIENT_SECRET, `GOOGLE_CLIENT_SECRET`);
   return (
     <main className="pt-14 relative">
-      Better auth env : {process.env.BETTER_AUTH_URL}
-      Google env : {process.env.GOOGLE_CLIENT_SECRET}
       <Hero />
       <section className="div-center-md">
         <AdminMailAlert />
