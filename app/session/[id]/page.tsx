16c7ca40c0223677800291989208af7d409c608e
Khawar Sultan <khawarsultan663@gmail.com>
2025-08-30 16:24:08 +0500

    Update session page logic to redirect users without a valid session; improved user flow by ensuring only authenticated users can access session data.

--- app/session/[id]/page.tsx
+++ app/session/[id]/page.tsx
@@ -1,15 +1,16 @@
 import { getSessionById } from "@/actions/saved-session-actions"
 import { getUser } from "@/actions/users-actions";
 import { SessionPageComponent } from "@/components/aiag-components/session/session-component";
-import { notFound } from "next/navigation"
+import { notFound, redirect } from "next/navigation"
 
 const page = async ({ params }: any) => {
     const resolvedParams = await params;
     const sessionId = resolvedParams?.id;
     const sessionData = await getSessionById(sessionId)
     const user = await getUser()
-    if (!sessionData) {
-        notFound()
+    
+    if (!user) {
+        redirect('/')
     }
     return <SessionPageComponent user={user} initialData={sessionData} />
 }
