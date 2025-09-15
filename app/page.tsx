c635b3f03cd0b20d103d7c626d81c8389a38d555
Khawar Sultan <khawarsultan663@gmail.com>
2025-09-15 22:12:54 +0500

    Remove direct usage of CLAUDE_API_KEY in HomePage component for improved security and maintainability. Streamline code by eliminating unnecessary variable declaration.

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,10 @@ import { Hero } from "@/components/aiag-components/reusable-components/hero"
 import AdminMailAlert from "@/components/aiag-components/admin-mail-alert"
 import HomePageComponent from "@/components/aiag-components/home/home-page-component"
 export default function HomePage() {
-  const CLAUDE_API_KEY = process.env.CLAUDE_API_KEY!;
   return (
     <main className="pt-14 relative">
       <Hero />
       <section className="div-center-md">
-      {CLAUDE_API_KEY}
         <AdminMailAlert />
         <HomePageComponent />
       </section>
