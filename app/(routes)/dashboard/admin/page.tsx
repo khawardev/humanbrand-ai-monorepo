dfd1a31546148b4cb1e6a4fe4b14eaa45dbc2742
Khawar Sultan <khawarsultan663@gmail.com>
2026-02-03 14:12:47 +0500

    update: Added Company emails in Admin page

--- app/(routes)/dashboard/admin/page.tsx
+++ app/(routes)/dashboard/admin/page.tsx
@@ -1,11 +1,11 @@
-import { getAllUsers } from '@/server/actions/usersActions';
+import { getAllUsers, getCompanyUsers } from '@/server/actions/usersActions';
 import { getAllSupportTickets } from '@/server/actions/supportActions';
 import { DashboardInnerLayout } from '@/components/shared/DashboardComponents';
 import { AdminDashboard } from '@/components/routes/admin/AdminDashboard';
 import { Hero } from '@/components/shared/reusable/Hero';
 
 const AdminPage = async () => {
-    const users = await getAllUsers();
+    const users = await getCompanyUsers();
     const tickets = await getAllSupportTickets();
 
     return (
